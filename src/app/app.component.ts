5a38968050b909be89397334c036700aa38dc8b3
wael-hlali <flashwael@gmail.com>
2023-04-27 18:27:54 +0100

    tp

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TP';
+
+  teamName = 'liverpool fc';
+  coachName = 'jürgen klopp';
+
+  sectionApplied = true;
+
+  para = "text-align: center;color: #C8102E;";
+
+}
